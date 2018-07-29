eeb591cc2722d5fec4cbd731ed4e23e3c718fe5c
Kudzai Nyandoro <kudzain@gmail.com>
2018-07-29 07:07:46 -0700

    Add daily averages with alias.

--- insert_studio_hours.sql
+++ insert_studio_hours.sql
@@ -48,13 +48,20 @@ insert into studio_hours(month, week, sunday_hours, monday_hours, tuesday_hours,
 values('June',5,3,5,4.25,6,3.25,4.75,2.5);
 
 -- July 2018
+-- Week 1
 insert into studio_hours(month, week, sunday_hours, monday_hours, tuesday_hours, wednesday_hours, thursday_hours, friday_hours, saturday_hours)
 values('July',1,4,2,3,7,1,3.25,9.75);
 
+-- Week 2
 insert into studio_hours(month, week, sunday_hours, monday_hours, tuesday_hours, wednesday_hours, thursday_hours, friday_hours, saturday_hours)
 values('July',2,7,5,3.75,6.5,6,5,10);
 
+-- Week 3
 insert into studio_hours(month, week, sunday_hours, monday_hours, tuesday_hours, wednesday_hours, thursday_hours, friday_hours, saturday_hours)
-values('July',3,7,6,5,4.25,0,0,0);
+values('July',3,7,6,5,4.25,4.75,6.5,6);
+
+-- Week 4
+insert into studio_hours(month, week, sunday_hours, monday_hours, tuesday_hours, wednesday_hours, thursday_hours, friday_hours, saturday_hours)
+values('July',4,4.5,4.25,3.75,6.25,6,4,5.5);
 
 
