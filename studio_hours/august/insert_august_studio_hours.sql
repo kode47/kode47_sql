e5496eab0c545b217ecaa64d143a41bde6dabf92
Kudzai Nyandoro <kudzain@gmail.com>
2018-08-20 04:21:02 -0700

    Update August Week 3 Studio Hours.

--- studio_hours/august/insert_august_studio_hours.sql
+++ studio_hours/august/insert_august_studio_hours.sql
@@ -6,3 +6,7 @@ values('August',1,DEFAULT,DEFAULT,DEFAULT,5,5,5,9.5);
 -- Week 2
 insert into august_studio_hours(month, week, sunday_hours, monday_hours, tuesday_hours, wednesday_hours, thursday_hours, friday_hours, saturday_hours)
 values('August',2,12.5,6,8.25,5.25,6,5.25,8.00);
+
+-- Week 3
+insert into august_studio_hours(month, week, sunday_hours, monday_hours, tuesday_hours, wednesday_hours, thursday_hours, friday_hours, saturday_hours)
+values('August', 3, 9.50, 5.50, 6.00, 6.00, 5.00, 6.00, 10.50);
