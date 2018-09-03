6491fba9d5cd9cc8b2711adbbd16f6bb287e447b
Kudzai Nyandoro <kudzain@gmail.com>
2018-09-03 12:06:13 -0700

    Update August Hours and Add September folder.

--- studio_hours/august/insert_august_studio_hours.sql
+++ studio_hours/august/insert_august_studio_hours.sql
@@ -10,3 +10,11 @@ values('August',2,12.5,6,8.25,5.25,6,5.25,8.00);
 -- Week 3
 insert into august_studio_hours(month, week, sunday_hours, monday_hours, tuesday_hours, wednesday_hours, thursday_hours, friday_hours, saturday_hours)
 values('August', 3, 9.50, 5.50, 6.00, 6.00, 5.00, 6.00, 10.50);
+
+-- Week 4
+insert into august_studio_hours(month, week, sunday_hours, monday_hours, tuesday_hours, wednesday_hours, thursday_hours, friday_hours, saturday_hours)
+values('August', 4, 10.50, 5.50, 6.25, 6.00, 5.00, 6.00, 6.00);
+
+-- Week 5
+insert into august_studio_hours(month, week, sunday_hours, monday_hours, tuesday_hours, wednesday_hours, thursday_hours, friday_hours, saturday_hours)
+values('August', 5, 5.50, 6.00, 8.25, 7.00, 2.50, 2.50, DEFAULT);
