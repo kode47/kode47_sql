c7ed21e4fb793d1bbd2c84358ad6833094aff875
Kudzai Nyandoro <kudzain@gmail.com>
2018-11-01 02:56:09 -0700

    Update the September and October 2018 studio hours.

---
+++ studio_hours/october/insert_october_studio_hours.sql
@@ -0,0 +1,24 @@
+-- Week 1
+-- October
+
+DELETE FROM october_studio_hours;
+
+insert into october_studio_hours(month, week, sunday_hours, monday_hours, tuesday_hours, wednesday_hours, thursday_hours, friday_hours, saturday_hours)
+values('October',1,DEFAULT,6.00,6.00,6.50,5.00,5.50,6.00);
+
+-- Week 2
+insert into october_studio_hours(month, week, sunday_hours, monday_hours, tuesday_hours, wednesday_hours, thursday_hours, friday_hours, saturday_hours)
+values('October',2,5.00,3.00,7.00,6.00,6.00,6.00,5.50);
+
+-- Week 3
+insert into october_studio_hours(month, week, sunday_hours, monday_hours, tuesday_hours, wednesday_hours, thursday_hours, friday_hours, saturday_hours)
+values('October',3,1.00,5.00,5.50,5.00,6.50,6.50,6.50);
+
+-- Week 4
+insert into october_studio_hours(month, week, sunday_hours, monday_hours, tuesday_hours, wednesday_hours, thursday_hours, friday_hours, saturday_hours)
+values('October',4,2.00,5.00,7.00,6.50,7.00,5.00,5.50);
+
+-- Week 5
+insert into october_studio_hours(month, week, sunday_hours, monday_hours, tuesday_hours, wednesday_hours, thursday_hours, friday_hours, saturday_hours)
+values('October',5,6.00,5.00,7.00,6.00,DEFAULT,DEFAULT,DEFAULT);
+
--- studio_hours/september/insert_september_studio_hours.sql
+++ studio_hours/september/insert_september_studio_hours.sql
@@ -7,5 +7,21 @@ insert into september_studio_hours(month, week, sunday_hours, monday_hours, tues
 values('September',1,DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT,5.75);
 
 -- Week 2
---insert into august_studio_hours(month, week, sunday_hours, monday_hours, tuesday_hours, wednesday_hours, thursday_hours, friday_hours, saturday_hours)
---values('September',2,12.5,6,8.25,5.25,6,5.25,8.00);
+insert into september_studio_hours(month, week, sunday_hours, monday_hours, tuesday_hours, wednesday_hours, thursday_hours, friday_hours, saturday_hours)
+values('September',2,6.00,8.50,5.00,5.00,2.00,2.00,2.00);
+
+-- Week 3
+insert into september_studio_hours(month, week, sunday_hours, monday_hours, tuesday_hours, wednesday_hours, thursday_hours, friday_hours, saturday_hours)
+values('September',3,2.00,2.00,2.00,2.00,2.00,2.00,7.00);
+
+-- Week 4
+insert into september_studio_hours(month, week, sunday_hours, monday_hours, tuesday_hours, wednesday_hours, thursday_hours, friday_hours, saturday_hours)
+values('September',4,0.00,2.50,5.00,5.00,5.00,2.00,2.00);
+
+-- Week 5
+insert into september_studio_hours(month, week, sunday_hours, monday_hours, tuesday_hours, wednesday_hours, thursday_hours, friday_hours, saturday_hours)
+values('September',5,2.00,2.00,2.00,4.00,5.00,5.00,9.00);
+
+-- Week 6
+insert into september_studio_hours(month, week, sunday_hours, monday_hours, tuesday_hours, wednesday_hours, thursday_hours, friday_hours, saturday_hours)
+values('September',6,8.00,DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT,DEFAULT);
